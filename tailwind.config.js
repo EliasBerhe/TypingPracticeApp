887b0d826e02284d52fcf429d8dea8dba8c1464a
EliasBerhe <eliastes123@gmial.com>
2023-03-25 12:54:15 -0400

    initialized project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    backgroundSize: {
+      'auto': 'auto',
+      'cover': 'cover',
+      'contain': 'contain',
+      '50%': '50%',
+      '16': '4rem',
+    }
+  },
+  plugins: [],
+}
